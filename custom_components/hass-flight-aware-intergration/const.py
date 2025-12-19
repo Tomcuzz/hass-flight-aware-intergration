d78c23f961519de3e5a4dd2bb4c1bbf92105b66e
Thomas Cousin <907403+Tomcuzz@users.noreply.github.com>
2025-12-19 02:07:18 +0100

    Update const.py

--- custom_components/hass-flight-aware-intergration/const.py
+++ custom_components/hass-flight-aware-intergration/const.py
@@ -3,7 +3,8 @@ from homeassistant.const import Platform
 DOMAIN = "hacs_flight_aware_intergration"
 
 PLATFORMS = [
-    "sensor"
+    "sensor",
+    "input_text"
 ]
 
 DEFAULT_SCAN_INTERVAL = 300 # 5 minutes in seconds
