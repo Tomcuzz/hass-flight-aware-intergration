502b8d032079b22710a11d8d192fd5e80bc50687
Thomas Cousin <907403+Tomcuzz@users.noreply.github.com>
2025-12-19 02:06:38 +0100

    Create input_text.py

---
+++ custom_components/hass-flight-aware-intergration/input_text.py
@@ -0,0 +1,20 @@
+# --- Platform Setup ---
+async def async_setup_entry(hass, entry, async_add_entities):
+    """Set up the input_text platform."""
+  
+    async_add_entities([
+        FlightAwarePredictedFlightInput()
+    ], True)
+
+class FlightAwarePredictedFlightInput(TextEntity):
+    # Implement one of these methods.
+    def __init__(self):
+        """Initialize the sensor."""
+        self._attr_name = "Predicted Flight Input"
+        self._attr_unique_id = f"flight_number_to_track"
+        self._attr_icon = "mdi:airplane-takeoff"
+        self._attr_native_value = ""
+
+    async def async_set_value(self, value: str) -> None:
+        """Set the text value."""
+        self._attr_native_value = value
