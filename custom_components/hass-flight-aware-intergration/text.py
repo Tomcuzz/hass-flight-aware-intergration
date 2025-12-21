e984e057d228c482e9db0a4d0b44cfce6a0238ff
Thomas Cousin <907403+Tomcuzz@users.noreply.github.com>
2025-12-21 12:43:16 +0100

    Update text.py

--- custom_components/hass-flight-aware-intergration/text.py
+++ custom_components/hass-flight-aware-intergration/text.py
@@ -16,7 +16,20 @@ class FlightAwarePredictedFlightInput(RestoreText):
         self._attr_unique_id = f"flight_number_to_track"
         self._attr_icon = "mdi:airplane-takeoff"
         self._attr_native_value = ""
+    
+    def set_value(self, value: str) -> None:
+        """Set the text value."""
+        self._attr_native_value = value
 
     async def async_set_value(self, value: str) -> None:
         """Set the text value."""
         self._attr_native_value = value
+        
+    async def async_added_to_hass(self) -> None:
+        await super().async_added_to_hass()
+        if (last_text_data := await self.async_get_last_text_data()) is None:
+            return
+        _LOGGER.debug("Restored state: %s", last_text_data)
+        self._attr_native_max = last_text_data.native_max
+        self._attr_native_min = last_text_data.native_min
+        self._attr_native_value = last_text_data.native_value
